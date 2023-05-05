f2253bafc5af51d6a403eda780d13438a66d9a0d
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-05-05 22:44:12 +0530

    added : int num

---
+++ src/com/ks/basic/PrintInt.java
@@ -0,0 +1,12 @@
+package com.ks.basic;
+import java.util.Scanner;
+public class PrintInt {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int num = sc.nextInt();
+        System.out.println("your enterd num :"+num);
+    }
+}
