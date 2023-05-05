23ba7ef3f1bd8a4c0c14229df303fe92befdf6b4
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-05-05 22:49:45 +0530

    added : int sum  num

--- src/com/ks/basic/PrintInt.java
+++ src/com/ks/basic/PrintInt.java
@@ -5,8 +5,11 @@ public class PrintInt {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number");
-        int num = sc.nextInt();
-        System.out.println("your enterd num :"+num);
+        System.out.println("Enter First NUm");
+        int num1 = sc.nextInt();
+        System.out.println("enter second num");
+        int num2 = sc.nextInt();
+        int sum = num1+num2;
+        System.out.println("your enterd num :"+sum);
     }
 }
