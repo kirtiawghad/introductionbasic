1dca9be459ecc0b1de070da615fef9e89f2e72c5
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-05-05 23:02:34 +0530

    added : practice problem

---
+++ src/com/ks/basic/PracticeProblem.java
@@ -0,0 +1,21 @@
+package com.ks.basic;
+
+public class PracticeProblem {
+
+    public static void main(String[] args) {
+
+
+        char ch = 'a';
+        System.out.println(ch);
+        int ascii =ch;
+        System.out.println(ascii);
+
+        int castacsii = (int) ch;
+        System.out.println(castacsii);
+
+        //type casting
+
+
+
+    }
+}
